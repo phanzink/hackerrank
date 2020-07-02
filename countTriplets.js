3a5558f9b0f90cd597348fd7213bc8dedc8f4891
phanzink <phanzink@gmail.com>
2020-07-02 06:51:21 -0500

    added maps and only one loop

--- countTriplets.js
+++ countTriplets.js
@@ -2,20 +2,20 @@
 function countTriplets(arr, r) {
     // console.log(arr, r);
     let countPairs = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (i - arr.length < 2) {
-            for (let j = i+1; j < arr.length - 1; j++) {
-                //check if it matches the rule
-               if ((arr[i] * r) == arr[j]) {
-                    for (let k = j+1; k < arr.length; k++) {
-                        if((arr[j] * r) == arr[k]) {
-                            countPairs = countPairs + 1;
-                        }
-                    }
-                }
-            }
+    let hMap2 = {};
+    let hMap3 = {};
+
+    arr.forEach(val => {
+        if (hMap3.hasOwnProperty(val)) {
+            countPairs = countPairs + hMap3[val]
         }
-    }
+
+        if (hMap2.hasOwnProperty(val)) {
+            hMap3[val*r] = (hMap3[val*r] = hMap3[val*r] + hMap2[val]) || hMap2[val];
+        }
+
+        hMap2[val*r] = (hMap2[val*r] = hMap2[val*r] + 1) || 1;
+    });
 
     return countPairs;
 }
